fc8ac2d82dd6d0aa6d581bb64916c4b18e63288a
Eden <l7w3gcww@gmail.com>
2025-09-26 15:34:51 +0800

    更改算術邏輯 -> 美金換台幣/台幣換美金

--- AJAX/AJAX/WebFront/obj/Debug/net8.0/WebFront.AssemblyInfo.cs
+++ AJAX/AJAX/WebFront/obj/Debug/net8.0/WebFront.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebFront")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b9d9551c6126e52a8157206bf1c1967eb7407bb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b9ded79337a98d6d3e1a11f9206444cfc3f28fc")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebFront")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebFront")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
