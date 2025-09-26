502eed52d981d401cb62360e8d524632836b04e1
Eden <l7w3gcww@gmail.com>
2025-09-26 15:10:47 +0800

    分三區

--- AJAX/AJAX/WebFront/obj/Debug/net8.0/WebFront.AssemblyInfo.cs
+++ AJAX/AJAX/WebFront/obj/Debug/net8.0/WebFront.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebFront")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da0585849cc78f0f43bbf18a4a90a205d3a40368")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2477f3a6910d1416fca03bb3f34c8c03f928860c")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebFront")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebFront")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
