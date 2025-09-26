4a8ad64cdeeb5b78651901103a5e02e1e46f1b37
Eden <l7w3gcww@gmail.com>
2025-09-26 13:46:31 +0800

    寫到MVC中

--- AJAX/AJAX/WebFront/Controllers/HomeController.cs
+++ AJAX/AJAX/WebFront/Controllers/HomeController.cs
@@ -44,7 +44,14 @@ namespace WebFront.Controllers
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		// GET: Home/Basic
+		[HttpGet]
+		public IActionResult Basic()
+		{
+			return View();
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
