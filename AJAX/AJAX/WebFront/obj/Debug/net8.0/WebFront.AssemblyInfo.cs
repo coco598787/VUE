dc71517306b2f3344f5469dd2de1789dbd1d3532
Eden <l7w3gcww@gmail.com>
2025-09-26 14:43:40 +0800

    解BUG，程式要有樣式區跟程式區

--- AJAX/AJAX/WebFront/obj/Debug/net8.0/WebFront.AssemblyInfo.cs
+++ AJAX/AJAX/WebFront/obj/Debug/net8.0/WebFront.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebFront")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a8ad64cdeeb5b78651901103a5e02e1e46f1b37")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+810d68d8f8c23b45103f8e123ae24e719773cddb")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebFront")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebFront")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
