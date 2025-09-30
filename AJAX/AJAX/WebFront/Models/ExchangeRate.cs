49358ef78137e20349bc93dc7921c5f8bfea942c
Eden <l7w3gcww@gmail.com>
2025-09-30 09:24:52 +0800

    編輯 - 選擇性貼上 - JSON貼上

--- AJAX/AJAX/WebFront/Models/ExchangeRate.cs
+++ AJAX/AJAX/WebFront/Models/ExchangeRate.cs
@@ -1,6 +1,19 @@
 ﻿namespace WebFront.Models
 {
-	public class ExchangeRate
+
+	public class Rootobject
 	{
+		public string Date { get; set; }
+		public string USDNTD { get; set; }
+		public string RMBNTD { get; set; }
+		public string EURUSD { get; set; }
+		public string USDJPY { get; set; }
+		public string GBPUSD { get; set; }
+		public string AUDUSD { get; set; }
+		public string USDHKD { get; set; }
+		public string USDRMB { get; set; }
+		public string USDZAR { get; set; }
+		public string NZDUSD { get; set; }
 	}
+
 }
