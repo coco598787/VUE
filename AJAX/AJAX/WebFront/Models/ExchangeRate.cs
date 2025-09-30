a2fdb5709fb4a075c8fdf5d86a3ba50e5660691d
Eden <l7w3gcww@gmail.com>
2025-09-30 09:31:55 +0800

    JSON屬性 & C#屬性，名稱要相同

--- AJAX/AJAX/WebFront/Models/ExchangeRate.cs
+++ AJAX/AJAX/WebFront/Models/ExchangeRate.cs
@@ -1,10 +1,15 @@
-﻿namespace WebFront.Models
+﻿using System.Text.Json.Serialization;	//新的JSON反序列化類別
+
+namespace WebFront.Models
 {
 
 	public class Rootobject
 	{
 		public string Date { get; set; }
-		public string USDNTD { get; set; }
+
+		//屬性名稱包含斜線，使用JsonPropertyName屬性來指定JSON中的名稱
+		[JsonPropertyName("USD/NTD")]   //JSON屬性名稱
+		public string USDNTD { get; set; } //C#屬性名稱
 		public string RMBNTD { get; set; }
 		public string EURUSD { get; set; }
 		public string USDJPY { get; set; }
