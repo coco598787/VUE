d96b719b564fb5f81cb2e9335c278399f382ce61
Eden <l7w3gcww@gmail.com>
2025-10-02 09:26:46 +0800

    調整div內容

--- AJAX/AJAX/WebFront/obj/Debug/net8.0/WebFront.AssemblyInfo.cs
+++ AJAX/AJAX/WebFront/obj/Debug/net8.0/WebFront.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebFront")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9872438b0d1e9c13f2592f9dcf32f4c3944c1e70")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1728cd9cd0ee2814ffb5e2baaf4a1aa6bd8e412c")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebFront")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebFront")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
