4dc73fa870357803011f8438d912a075268aad5b
Eden <l7w3gcww@gmail.com>
2025-10-02 09:31:33 +0800

    加一個事件

--- AJAX/AJAX/WebFront/obj/Debug/net8.0/WebFront.AssemblyInfo.cs
+++ AJAX/AJAX/WebFront/obj/Debug/net8.0/WebFront.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebFront")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1728cd9cd0ee2814ffb5e2baaf4a1aa6bd8e412c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a39ed8a3d8b734869ba27e86509dbb39c1753037")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebFront")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebFront")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
